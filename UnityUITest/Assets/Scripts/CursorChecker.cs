3a9c4574d3436aec7b5b2ea8abad9783d53f04a3
MichaelCourter <michael.courter.64@gmail.com>
2020-10-15 12:46:50 -0700

    Added the CursorChecker script to test GraphicRaycaster.Raycast().
    UiUnderCursor scene: added an empty GameObject that holds the new CursorChecker.
    
    CursorChecker: added to check for a mouse button 0 click to find all the UI GameObjects under the cursor at that moment.

---
+++ UnityUITest/Assets/Scripts/CursorChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Checks for a mouse button 0 click to find all the UI GameObjects under the cursor at that moment.
+/// </summary>
+public class CursorChecker : MonoBehaviour
+{
+    [SerializeField] private GraphicRaycaster graphicRaycaster;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PointerEventData currentCursorEvent = new PointerEventData(null);
+            currentCursorEvent.position = Input.mousePosition;
+
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+
+            string raycastsAsLog = "Found GameObjects: ";
+
+            graphicRaycaster.Raycast(currentCursorEvent, raycastResults);
+
+            foreach (RaycastResult result in raycastResults)
+            {
+                raycastsAsLog += string.Format("{0} / ", result.gameObject.name);
+            }
+
+            Debug.Log(raycastsAsLog);
+        }
+    }
+}
