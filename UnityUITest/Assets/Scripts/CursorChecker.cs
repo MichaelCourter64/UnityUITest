80b84e9105b2cda446a5a499707f36e7478c631e
MichaelCourter <michael.courter.64@gmail.com>
2020-10-15 16:03:44 -0700

    Refactored CursorChecker to improve modularity and readability.

--- UnityUITest/Assets/Scripts/CursorChecker.cs
+++ UnityUITest/Assets/Scripts/CursorChecker.cs
@@ -8,27 +8,49 @@ using UnityEngine.UI;
 /// </summary>
 public class CursorChecker : MonoBehaviour
 {
+    [Tooltip("Usually found in a Canvas GameObject")]
     [SerializeField] private GraphicRaycaster graphicRaycaster;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            PointerEventData currentCursorEvent = new PointerEventData(null);
-            currentCursorEvent.position = Input.mousePosition;
-
-            List<RaycastResult> raycastResults = new List<RaycastResult>();
-
-            string raycastsAsLog = "Found GameObjects: ";
-
-            graphicRaycaster.Raycast(currentCursorEvent, raycastResults);
-
-            foreach (RaycastResult result in raycastResults)
-            {
-                raycastsAsLog += string.Format("{0} / ", result.gameObject.name);
-            }
-
-            Debug.Log(raycastsAsLog);
+            FindUiUnderMouseCursorAndLog();
         }
+    }
+
+    private void FindUiUnderMouseCursorAndLog()
+    {
+        List<RaycastResult> uiRaycastResults = FindUiElementsUnderPointer(Input.mousePosition, graphicRaycaster);
+
+        string uiRaycastResultsLog = RaycastGameObjectResultsToLogString(uiRaycastResults);
+
+        Debug.Log(uiRaycastResultsLog);
+    }
+
+    private List<RaycastResult> FindUiElementsUnderPointer(Vector3 pointerPosition, GraphicRaycaster graphicRaycaster)
+    {
+        PointerEventData currentCursorEvent = new PointerEventData(null)
+        {
+            position = pointerPosition
+        };
+
+        List<RaycastResult> uiRaycastResults = new List<RaycastResult>();
+
+        graphicRaycaster.Raycast(currentCursorEvent, uiRaycastResults);
+
+        return uiRaycastResults;
+    }
+
+    private string RaycastGameObjectResultsToLogString(List<RaycastResult> raycastResults)
+    {
+        string raycastGameObjectsAsLog = "Found GameObjects: ";
+
+        foreach (RaycastResult result in raycastResults)
+        {
+            raycastGameObjectsAsLog += string.Format("{0} / ", result.gameObject.name);
+        }
+
+        return raycastGameObjectsAsLog;
     }
 }
